3d77f319a2320fe1a61383829558fd943f027330
Nguyen Thanh Dat <datdatnguyen2609@gmail.com>
2025-09-29 17:10:35 +0700

    update

--- i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/xsim_webtalk.tcl
+++ i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Sep 29 11:31:27 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Sep 29 16:14:58 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "d8be9ddc-4904-4d8e-ac2d-f9766af5ab42" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "bd3ea96680974ebf88e6e6598e2bb076" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "42" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "44" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 5500U with Radeon Graphics         " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "50 ms" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "9348_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4234935483 -regid "" -xml C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "9364_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1319154073 -regid "" -xml C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/datda/Downloads/jit_intern/i2c_controller/i2c_controller.sim/sim_1/behav/xsim/xsim.dir/i2c_master_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
