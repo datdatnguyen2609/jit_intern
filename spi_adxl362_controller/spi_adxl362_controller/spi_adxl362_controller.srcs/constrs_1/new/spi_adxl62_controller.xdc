500c3f4ead8048c54641338022c88a1786593821
Nguyen Thanh Dat <datdatnguyen2609@gmail.com>
2025-11-14 15:59:57 +0700

    fix

--- spi_adxl362_controller/spi_adxl362_controller/spi_adxl362_controller.srcs/constrs_1/new/spi_adxl62_controller.xdc
+++ spi_adxl362_controller/spi_adxl362_controller/spi_adxl362_controller.srcs/constrs_1/new/spi_adxl62_controller.xdc
@@ -10,40 +10,40 @@ create_clock -period 10.000 -name sys_clk_pin -waveform {0.000 5.000} -add [get_
 
 
 ##Switches
-set_property -dict {PACKAGE_PIN J15 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[0]}]
-set_property -dict {PACKAGE_PIN L16 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[1]}]
-set_property -dict {PACKAGE_PIN M13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[2]}]
-set_property -dict {PACKAGE_PIN R15 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[3]}]
-set_property -dict {PACKAGE_PIN R17 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[4]}]
-set_property -dict {PACKAGE_PIN T18 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[5]}]
-set_property -dict {PACKAGE_PIN U18 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[6]}]
-set_property -dict {PACKAGE_PIN R13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[7]}]
-set_property -dict {PACKAGE_PIN T8 IOSTANDARD LVCMOS18} [get_ports {top_i_sw[8]}]
-set_property -dict {PACKAGE_PIN U8 IOSTANDARD LVCMOS18} [get_ports {top_i_sw[9]}]
-set_property -dict {PACKAGE_PIN R16 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[10]}]
-set_property -dict {PACKAGE_PIN T13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[11]}]
-set_property -dict {PACKAGE_PIN H6 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[12]}]
-set_property -dict {PACKAGE_PIN U12 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[13]}]
-set_property -dict {PACKAGE_PIN U11 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[14]}]
-set_property -dict {PACKAGE_PIN V10 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[15]}]
+# set_property -dict {PACKAGE_PIN J15 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[0]}]
+# set_property -dict {PACKAGE_PIN L16 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[1]}]
+# set_property -dict {PACKAGE_PIN M13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[2]}]
+# set_property -dict {PACKAGE_PIN R15 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[3]}]
+# set_property -dict {PACKAGE_PIN R17 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[4]}]
+# set_property -dict {PACKAGE_PIN T18 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[5]}]
+# set_property -dict {PACKAGE_PIN U18 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[6]}]
+# set_property -dict {PACKAGE_PIN R13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[7]}]
+# set_property -dict {PACKAGE_PIN T8 IOSTANDARD LVCMOS18} [get_ports {top_i_sw[8]}]
+# set_property -dict {PACKAGE_PIN U8 IOSTANDARD LVCMOS18} [get_ports {top_i_sw[9]}]
+# set_property -dict {PACKAGE_PIN R16 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[10]}]
+# set_property -dict {PACKAGE_PIN T13 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[11]}]
+# set_property -dict {PACKAGE_PIN H6 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[12]}]
+# set_property -dict {PACKAGE_PIN U12 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[13]}]
+# set_property -dict {PACKAGE_PIN U11 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[14]}]
+# set_property -dict {PACKAGE_PIN V10 IOSTANDARD LVCMOS33} [get_ports {top_i_sw[15]}]
 
 ## LEDs
-set_property -dict {PACKAGE_PIN H17 IOSTANDARD LVCMOS33} [get_ports {top_o_din[0]}]
-set_property -dict {PACKAGE_PIN K15 IOSTANDARD LVCMOS33} [get_ports {top_o_din[1]}]
-set_property -dict {PACKAGE_PIN J13 IOSTANDARD LVCMOS33} [get_ports {top_o_din[2]}]
-set_property -dict {PACKAGE_PIN N14 IOSTANDARD LVCMOS33} [get_ports {top_o_din[3]}]
-set_property -dict {PACKAGE_PIN R18 IOSTANDARD LVCMOS33} [get_ports {top_o_din[4]}]
-set_property -dict {PACKAGE_PIN V17 IOSTANDARD LVCMOS33} [get_ports {top_o_din[5]}]
-set_property -dict {PACKAGE_PIN U17 IOSTANDARD LVCMOS33} [get_ports {top_o_din[6]}]
-set_property -dict {PACKAGE_PIN U16 IOSTANDARD LVCMOS33} [get_ports {top_o_din[7]}]
-#set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports { LED[8] }]; #IO_L16N_T2_A15_D31_14 Sch=led[8]
-#set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33 } [get_ports { LED[9] }]; #IO_L14N_T2_SRCC_14 Sch=led[9]
-#set_property -dict { PACKAGE_PIN U14   IOSTANDARD LVCMOS33 } [get_ports { LED[10] }]; #IO_L22P_T3_A05_D21_14 Sch=led[10]
-#set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { LED[11] }]; #IO_L15N_T2_DQS_DOUT_CSO_B_14 Sch=led[11]
-#set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { LED[12] }]; #IO_L16P_T2_CSI_B_14 Sch=led[12]
-#set_property -dict { PACKAGE_PIN V14   IOSTANDARD LVCMOS33 } [get_ports { LED[13] }]; #IO_L22N_T3_A04_D20_14 Sch=led[13]
-#set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { LED[14] }]; #IO_L20N_T3_A07_D23_14 Sch=led[14]
-#set_property -dict { PACKAGE_PIN V11   IOSTANDARD LVCMOS33 } [get_ports { LED[15] }]; #IO_L21N_T3_DQS_A06_D22_14 Sch=led[15]
+set_property -dict { PACKAGE_PIN H17   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[0] }]; #IO_L18P_T2_A24_15 Sch=led[0]
+set_property -dict { PACKAGE_PIN K15   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[1] }]; #IO_L24P_T3_RS1_15 Sch=led[1]
+set_property -dict { PACKAGE_PIN J13   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[2] }]; #IO_L17N_T2_A25_15 Sch=led[2]
+set_property -dict { PACKAGE_PIN N14   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[3] }]; #IO_L8P_T1_D11_14 Sch=led[3]
+set_property -dict { PACKAGE_PIN R18   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[4] }]; #IO_L7P_T1_D09_14 Sch=led[4]
+set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[5] }]; #IO_L18N_T2_A11_D27_14 Sch=led[5]
+set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[6] }]; #IO_L17P_T2_A14_D30_14 Sch=led[6]
+set_property -dict { PACKAGE_PIN U16   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[7] }]; #IO_L18P_T2_A12_D28_14 Sch=led[7]
+set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[8] }]; #IO_L16N_T2_A15_D31_14 Sch=led[8]
+set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[9] }]; #IO_L14N_T2_SRCC_14 Sch=led[9]
+set_property -dict { PACKAGE_PIN U14   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[10] }]; #IO_L22P_T3_A05_D21_14 Sch=led[10]
+set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[11] }]; #IO_L15N_T2_DQS_DOUT_CSO_B_14 Sch=led[11]
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[12] }]; #IO_L16P_T2_CSI_B_14 Sch=led[12]
+set_property -dict { PACKAGE_PIN V14   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[13] }]; #IO_L22N_T3_A04_D20_14 Sch=led[13]
+set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[14] }]; #IO_L20N_T3_A07_D23_14 Sch=led[14]
+set_property -dict { PACKAGE_PIN V11   IOSTANDARD LVCMOS33 } [get_ports { top_o_led[15] }]; #IO_L21N_T3_DQS_A06_D22_14 Sch=led[15]
 
 ##7 segment display
 #set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { CA }]; #IO_L24N_T3_A00_D16_14 Sch=ca
@@ -67,12 +67,11 @@ set_property -dict {PACKAGE_PIN U16 IOSTANDARD LVCMOS33} [get_ports {top_o_din[7
 #set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { CPU_RESETN }]; #IO_L3P_T0_DQS_AD1P_15 Sch=cpu_resetn
 
 ##Buttons
-#set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { BTNC }]; #IO_L9P_T1_DQS_14 Sch=btnc
-#set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { BTNU }]; #IO_L4N_T0_D05_14 Sch=btnu
-set_property -dict {PACKAGE_PIN P17 IOSTANDARD LVCMOS33} [get_ports top_i_mode]
-set_property -dict {PACKAGE_PIN M17 IOSTANDARD LVCMOS33} [get_ports top_i_rst]
-set_property -dict {PACKAGE_PIN P18 IOSTANDARD LVCMOS33} [get_ports top_i_ready]
-
+set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { top_i_btn_t }]; #IO_L9P_T1_DQS_14 Sch=btnc
+set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { top_i_btn_x }]; #IO_L4N_T0_D05_14 Sch=btnu
+set_property -dict { PACKAGE_PIN P17   IOSTANDARD LVCMOS33 } [get_ports { top_i_btn_y }]; #IO_L12P_T1_MRCC_14 Sch=btnl
+set_property -dict { PACKAGE_PIN M17   IOSTANDARD LVCMOS33 } [get_ports { top_i_rst }]; #IO_L10N_T1_D15_14 Sch=btnr
+set_property -dict { PACKAGE_PIN P18   IOSTANDARD LVCMOS33 } [get_ports { top_i_btn_z }]; #IO_L9N_T1_DQS_D13_14 Sch=btnd
 
 ##Accelerometer
 set_property -dict {PACKAGE_PIN E15 IOSTANDARD LVCMOS33} [get_ports top_i_miso]
@@ -81,4 +80,3 @@ set_property -dict {PACKAGE_PIN F15 IOSTANDARD LVCMOS33} [get_ports top_o_sclk]
 set_property -dict {PACKAGE_PIN D15 IOSTANDARD LVCMOS33} [get_ports top_o_csn]
 #set_property -dict { PACKAGE_PIN B13   IOSTANDARD LVCMOS33 } [get_ports { ACL_INT[1] }]; #IO_L2P_T0_AD8P_15 Sch=acl_int[1]
 #set_property -dict { PACKAGE_PIN C16   IOSTANDARD LVCMOS33 } [get_ports { ACL_INT[2] }]; #IO_L20P_T3_A20_15 Sch=acl_int[2]
-
--- spi_adxl362_controller/spi_adxl362_controller/spi_adxl362_controller.srcs/sources_1/new/top.sv
+++ spi_adxl362_controller/spi_adxl362_controller/spi_adxl362_controller.srcs/sources_1/new/top.sv
@@ -1,91 +1,93 @@
-// ==============================================
-// top.v (simple)
-// - READY giu muc den khi CSN len (giao dich xong)
-// - LED: WRITE -> hien ngay data; READ -> khi o_din_valid
-// ==============================================
 `timescale 1ns / 1ps
+
 module top (
     input         top_i_clk,
-    input         top_i_rst,    // nut reset chua loc (muc cao = reset)
-    input         top_i_ready,  // nut start
-    input         top_i_mode,   // 1=WRITE, 0=READ
+    input         top_i_rst,      // nút reset (m?c cao = reset)
+
+    input         top_i_btn_x,    // ??c X: reg 0x0E (L), 0x0F (H)
+    input         top_i_btn_y,    // ??c Y: reg 0x10 (L), 0x11 (H)
+    input         top_i_btn_z,    // ??c Z: reg 0x12 (L), 0x13 (H)
+    input         top_i_btn_t,    // ??c TEMP: reg 0x14 (L), 0x15 (H)
 
     output        top_o_csn,
     output        top_o_sclk,
     output        top_o_mosi,
     input         top_i_miso,
 
-    output [7:0]  top_o_din,    // 8 LED don
-    input  [15:0] top_i_sw      // [15:8]=DATA, [7:0]=ADDR
+    output [15:0] top_o_led       // 16 LED ??n
 );
 
-    // =========================
-    // 1) Debounce reset / ready / mode
-    // =========================
+    // =========================================
+    // 1) Debounce reset + 4 nút b?m
+    // =========================================
     wire w_rst_sync;
     debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_rst (
-        .I_clk(top_i_clk), .I_rst(1'b0), .I_btn_in(top_i_rst), .O_btn_out(w_rst_sync)
+        .I_clk    (top_i_clk),
+        .I_rst    (1'b0),
+        .I_btn_in (top_i_rst),
+        .O_btn_out(w_rst_sync)
     );
 
-    wire w_ready_db, w_mode_db;
-    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_ready (
-        .I_clk(top_i_clk), .I_rst(w_rst_sync), .I_btn_in(top_i_ready), .O_btn_out(w_ready_db)
+    wire w_btn_x_db, w_btn_y_db, w_btn_z_db, w_btn_t_db;
+
+    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_btn_x (
+        .I_clk    (top_i_clk),
+        .I_rst    (w_rst_sync),
+        .I_btn_in (top_i_btn_x),
+        .O_btn_out(w_btn_x_db)
     );
-    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_mode (
-        .I_clk(top_i_clk), .I_rst(w_rst_sync), .I_btn_in(top_i_mode), .O_btn_out(w_mode_db)
+    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_btn_y (
+        .I_clk    (top_i_clk),
+        .I_rst    (w_rst_sync),
+        .I_btn_in (top_i_btn_y),
+        .O_btn_out(w_btn_y_db)
+    );
+    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_btn_z (
+        .I_clk    (top_i_clk),
+        .I_rst    (w_rst_sync),
+        .I_btn_in (top_i_btn_z),
+        .O_btn_out(w_btn_z_db)
+    );
+    debounce #(.CLK_FREQ(100_000_000), .DEBOUNCE_TIME_MS(20)) u_db_btn_t (
+        .I_clk    (top_i_clk),
+        .I_rst    (w_rst_sync),
+        .I_btn_in (top_i_btn_t),
+        .O_btn_out(w_btn_t_db)
     );
 
-    // canh len cua READY sau debounce
-    reg r_ready_q;
-    always @(posedge top_i_clk) begin
-        if (w_rst_sync) r_ready_q <= 1'b0;
-        else            r_ready_q <= w_ready_db;
-    end
-    wire w_ready_edge = w_ready_db & ~r_ready_q;
-
-    // =========================
-    // 2) SPI controller
-    // =========================
-    wire        w_csn;
-    wire [7:0]  w_din;
-    wire        w_din_valid;
-
-    // tham so latched cho moi giao dich
-    reg  [7:0] r_addr_l, r_data_l;
-    reg        r_mode_l;      // 1=WRITE, 0=READ
-    reg        r_req;         // giu muc den khi CSN len
-
-    // phat req khi bam READY o luc CSN dang high (khong ban)
-    // xoa req khi CSN len lai
-    reg r_csn_q;
+    // C?nh lên c?a các nút sau debounce
+    reg r_btn_x_q, r_btn_y_q, r_btn_z_q, r_btn_t_q;
     always @(posedge top_i_clk) begin
         if (w_rst_sync) begin
-            r_csn_q   <= 1'b1;
-            r_req     <= 1'b0;
-            r_addr_l  <= 8'h00;
-            r_data_l  <= 8'h00;
-            r_mode_l  <= 1'b0;
+            r_btn_x_q <= 1'b0;
+            r_btn_y_q <= 1'b0;
+            r_btn_z_q <= 1'b0;
+            r_btn_t_q <= 1'b0;
         end else begin
-            r_csn_q <= w_csn;
-
-            // khoi tao giao dich
-            if (w_ready_edge && w_csn) begin
-                r_req    <= 1'b1;
-                r_addr_l <= top_i_sw[7:0];
-                r_data_l <= top_i_sw[15:8];
-                r_mode_l <= w_mode_db;
-            end
-
-            // ket thuc giao dich khi CSN len
-            if (~r_csn_q && w_csn) begin
-                r_req <= 1'b0;
-            end
+            r_btn_x_q <= w_btn_x_db;
+            r_btn_y_q <= w_btn_y_db;
+            r_btn_z_q <= w_btn_z_db;
+            r_btn_t_q <= w_btn_t_db;
         end
     end
 
-    // ma lenh theo mode da latched
-    wire [7:0] w_inst   = r_mode_l ? 8'h0A : 8'h0B; // WRITE/READ
-    wire       w_sel_rw = r_mode_l ? 1'b0  : 1'b1;  // 0=WRITE, 1=READ
+    wire w_btn_x_edge = w_btn_x_db & ~r_btn_x_q;
+    wire w_btn_y_edge = w_btn_y_db & ~r_btn_y_q;
+    wire w_btn_z_edge = w_btn_z_db & ~r_btn_z_q;
+    wire w_btn_t_edge = w_btn_t_db & ~r_btn_t_q;
+
+    // =========================================
+    // 2) SPI controller ADXL362
+    // =========================================
+    wire       w_csn;
+    wire [7:0] w_din;
+    wire       w_din_valid;
+
+    reg        r_spi_ready;
+    reg [7:0]  r_spi_inst;
+    reg        r_spi_sel_rw;   // 0=WRITE, 1=READ
+    reg [7:0]  r_spi_addr;
+    reg [7:0]  r_spi_dout;
 
     spi_adxl362_controller u_spi (
         .i_clk      (top_i_clk),
@@ -94,33 +96,179 @@ module top (
         .o_sclk     (top_o_sclk),
         .o_mosi     (top_o_mosi),
         .i_miso     (top_i_miso),
-        .i_ready    (r_req),          // level
-        .i_inst     (w_inst),
-        .i_sel_rw   (w_sel_rw),
-        .i_reg_addr (r_addr_l),
-        .i_dout     (r_data_l),
+
+        .i_ready    (r_spi_ready),   // pulse
+        .i_inst     (r_spi_inst),    // 0x0A WRITE, 0x0B READ
+        .i_sel_rw   (r_spi_sel_rw),  // 0=WRITE, 1=READ
+        .i_reg_addr (r_spi_addr),
+        .i_dout     (r_spi_dout),
         .o_din      (w_din),
         .o_din_valid(w_din_valid)
     );
 
-    // =========================
-    // 3) Hien thi LED
-    // =========================
-    reg [7:0] r_led;
+    assign top_o_csn = w_csn;
+
+    // Theo dõi c?nh CSN ?? bi?t giao d?ch xong
+    reg r_csn_q;
     always @(posedge top_i_clk) begin
-        if (w_rst_sync) r_led <= 8'h00;
-        else begin
-            // WRITE: cap nhat ngay khi bam nut (neu khong ban)
-            if (w_ready_edge && w_csn && w_mode_db) r_led <= top_i_sw[15:8];
-            // READ: chi cap nhat khi co din_valid
-            else if (w_din_valid && ~r_mode_l)      r_led <= w_din;
+        if (w_rst_sync) r_csn_q <= 1'b1;
+        else            r_csn_q <= w_csn;
+    end
+    wire w_csn_rise = (~r_csn_q) & w_csn;   // CSN 0->1
+
+    // =========================================
+    // 3) FSM ?i?u khi?n: 
+    // - init: ghi POWER_CTL (0x2D) = 0x02
+    // - sau ?ó: m?i nút ??c 2 byte (L,H) r?i hi?n th? lên LED
+    // =========================================
+    localparam S_INIT_POWER_START = 3'd0;
+    localparam S_INIT_POWER_WAIT  = 3'd1;
+    localparam S_IDLE             = 3'd2;
+    localparam S_READ_LOW_START   = 3'd3;
+    localparam S_READ_LOW_WAIT    = 3'd4;
+    localparam S_READ_HIGH_START  = 3'd5;
+    localparam S_READ_HIGH_WAIT   = 3'd6;
+
+    reg [2:0]  r_state;
+
+    localparam AXIS_X = 2'd0;
+    localparam AXIS_Y = 2'd1;
+    localparam AXIS_Z = 2'd2;
+    localparam AXIS_T = 2'd3;
+
+    reg [1:0]  r_axis_sel;
+    reg [15:0] r_axis_data;     // 16 bit ??c ???c (H:L)
+    reg [15:0] r_led_reg;       // d? li?u ??a ra LED
+
+    // base address cho byte th?p c?a t?ng kênh
+    function [7:0] axis_base_addr;
+        input [1:0] axis;
+        begin
+            case (axis)
+                AXIS_X: axis_base_addr = 8'h0E; // X_L
+                AXIS_Y: axis_base_addr = 8'h10; // Y_L
+                AXIS_Z: axis_base_addr = 8'h12; // Z_L
+                AXIS_T: axis_base_addr = 8'h14; // TEMP_L
+                default:axis_base_addr = 8'h0E;
+            endcase
+        end
+    endfunction
+
+    // FSM
+    always @(posedge top_i_clk) begin
+        if (w_rst_sync) begin
+            r_state       <= S_INIT_POWER_START;
+
+            r_spi_ready   <= 1'b0;
+            r_spi_inst    <= 8'h0A;   // m?c ??nh WRITE
+            r_spi_sel_rw  <= 1'b0;
+            r_spi_addr    <= 8'h00;
+            r_spi_dout    <= 8'h00;
+
+            r_axis_sel    <= AXIS_X;
+            r_axis_data   <= 16'h0000;
+            r_led_reg     <= 16'h0000;
+        end else begin
+            // m?c ??nh không phát READY
+            r_spi_ready <= 1'b0;
+
+            case (r_state)
+                //----------------------------------
+                // Kh?i ??ng: ghi POWER_CTL = 0x02
+                //----------------------------------
+                S_INIT_POWER_START: begin
+                    // Giao d?ch WRITE: INST=0x0A, sel_rw=0
+                    r_spi_inst    <= 8'h0A;      // WRITE
+                    r_spi_sel_rw  <= 1'b0;
+                    r_spi_addr    <= 8'h2D;      // POWER_CTL
+                    r_spi_dout    <= 8'h02;      // Measurement mode
+                    r_spi_ready   <= 1'b1;       // pulse 1 chu k?
+                    r_state       <= S_INIT_POWER_WAIT;
+                end
+
+                S_INIT_POWER_WAIT: begin
+                    // ??i giao d?ch xong (CSN lên l?i)
+                    if (w_csn_rise) begin
+                        r_state <= S_IDLE;
+                    end
+                end
+
+                //----------------------------------
+                // IDLE: ch? 4 nút
+                //----------------------------------
+                S_IDLE: begin
+                    if (w_btn_x_edge) begin
+                        r_axis_sel <= AXIS_X;
+                        r_state    <= S_READ_LOW_START;
+                    end else if (w_btn_y_edge) begin
+                        r_axis_sel <= AXIS_Y;
+                        r_state    <= S_READ_LOW_START;
+                    end else if (w_btn_z_edge) begin
+                        r_axis_sel <= AXIS_Z;
+                        r_state    <= S_READ_LOW_START;
+                    end else if (w_btn_t_edge) begin
+                        r_axis_sel <= AXIS_T;
+                        r_state    <= S_READ_LOW_START;
+                    end
+                end
+
+                //----------------------------------
+                // Giao d?ch ??c byte th?p (L)
+                //----------------------------------
+                S_READ_LOW_START: begin
+                    r_spi_inst    <= 8'h0B;              // READ
+                    r_spi_sel_rw  <= 1'b1;               // 1=READ
+                    r_spi_addr    <= axis_base_addr(r_axis_sel); // addr_L
+                    r_spi_dout    <= 8'h00;              // dummy
+                    r_spi_ready   <= 1'b1;               // start transaction
+                    r_state       <= S_READ_LOW_WAIT;
+                end
+
+                S_READ_LOW_WAIT: begin
+                    // L?u l?i byte th?p khi có din_valid
+                    if (w_din_valid) begin
+                        r_axis_data[7:0] <= w_din;       // LSB
+                    end
+                    // ??i CSN lên => xong giao d?ch
+                    if (w_csn_rise) begin
+                        r_state <= S_READ_HIGH_START;
+                    end
+                end
+
+                //----------------------------------
+                // Giao d?ch ??c byte cao (H)
+                //----------------------------------
+                S_READ_HIGH_START: begin
+                    r_spi_inst    <= 8'h0B;              // READ
+                    r_spi_sel_rw  <= 1'b1;
+                    r_spi_addr    <= axis_base_addr(r_axis_sel) + 8'd1; // addr_H
+                    r_spi_dout    <= 8'h00;
+                    r_spi_ready   <= 1'b1;
+                    r_state       <= S_READ_HIGH_WAIT;
+                end
+
+                S_READ_HIGH_WAIT: begin
+                    // L?u byte cao khi có din_valid
+                    if (w_din_valid) begin
+                        r_axis_data[15:8] <= w_din;      // MSB
+                    end
+                    // Khi CSN lên l?i => xong 2 byte, c?p nh?t LED
+                    if (w_csn_rise) begin
+                        r_led_reg <= r_axis_data;
+                        r_state   <= S_IDLE;
+                    end
+                end
+
+                default: begin
+                    r_state <= S_IDLE;
+                end
+            endcase
         end
     end
 
-    // =========================
-    // 4) Gan ra ngoai
-    // =========================
-    assign top_o_csn = w_csn;
-    assign top_o_din = r_led;
+    // =========================================
+    // 4) ??a d? li?u ra 16 LED
+    // =========================================
+    assign top_o_led = r_led_reg;
 
 endmodule
